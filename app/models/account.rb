52d92b497260ce8a387d6e6c70200bcd3234e7a6
Anthony Tilelli <Anthony@tilelli.me>
2020-05-29 21:56:51 -0400

    add account model validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :group
+
+  validates :name, uniqueness: { scope: :group, message: "is already taken in this group." }
+  validates :group, presence: true
 end
