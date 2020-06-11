20fa5a68575367c75ca34bcf1751130e979c173d
Anthony Tilelli <Anthony@tilelli.me>
2020-06-11 00:04:09 -0400

    added admin check

--- app/controllers/keychain_controller.rb
+++ app/controllers/keychain_controller.rb
@@ -1,17 +1,19 @@
 class KeychainController < ApplicationController
   before_action :login_required
   before_action :resolve_keychain # keep second
-  before_action :keychain_member_required
+  before_action :keychain_member_required # keep Third
+  # before_action :keychain_must_be_admin, only: [:create, :edit, :destory]
 
   def show; end
 
-  # TODO: create update (must have admin) edit (must have admin?) destroy (must have admin)
+  # TODO: create update (must have admin) edit (must have admin) destroy (must have admin)
 
   private
 
-  # Determines if user is an actice member of that keychain
+  # Determines if user is an active member of that keychain and sets @membership
   def keychain_member_required
-    return if @keychain.active_members.any? { |mem| mem.user == @user }
+    @membership = @keychain.active_members.find { |mem| mem.user == @user }
+    return if @membership
 
     flash[:alert] = ['Not an active member of that keychain']
     redirect_to login_path, status: :unauthorized
@@ -26,5 +28,12 @@ class KeychainController < ApplicationController
 
     flash[:alert] = ['Invalid Keychain requested']
     redirect_to login_path, status: :bad_request
+  end
+
+  def keychain_must_be_admin
+    return if @membership.admin
+
+    flash[:alert] = ['Must be an admin of the keychain to proceed']
+    redirect_to login_path, status: :unauthorized
   end
 end
