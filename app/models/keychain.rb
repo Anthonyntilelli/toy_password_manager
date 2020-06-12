65a1fff73fcb37005673199feb7a077a0bccc95f
Anthony Tilelli <Anthony@tilelli.me>
2020-06-11 21:20:52 -0400

    remove personal keychain data entry

--- app/models/keychain.rb
+++ app/models/keychain.rb
@@ -4,10 +4,6 @@ class Keychain < ApplicationRecord
   has_many :users, through: :memberships
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 200 }
-  validates :name, format: { without: /~/, message: '~ is reserved.' }, unless: -> { personal }
-  validates :name, format: { with: /~\d+/, message: 'personal keychains are ~user_id' }, if: -> { personal }
-  validates :personal, inclusion: { in: [true, false] } # Ensure boolean
-
   after_validation :normalize_name
 
   # Invite users to Keychain
--- db/migrate/20200529132948_create_keychains.rb
+++ db/migrate/20200529132948_create_keychains.rb
@@ -2,7 +2,6 @@ class CreateKeychains < ActiveRecord::Migration[6.0]
   def change
     create_table :keychains do |t|
       t.text :name, null: false
-      t.boolean :personal, null: false
 
       t.timestamps
     end
