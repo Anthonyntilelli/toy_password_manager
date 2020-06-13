0215c253ab81c7d9a25ff956ead0413f1645cf4c
Anthony Tilelli <Anthony@tilelli.me>
2020-06-13 01:48:32 -0400

    add create logic p1

--- app/controllers/keychain_controller.rb
+++ app/controllers/keychain_controller.rb
@@ -1,20 +1,41 @@
 class KeychainController < ApplicationController
   before_action :login_required
-  before_action :resolve_keychain, except: [:new] # keep second
-  before_action :keychain_member_required, except: [:new] # keep Third
-  # before_action :keychain_must_be_admin, only: [:create, :edit, :destory]
+  before_action :resolve_keychain, except: [:new, :create] # keep second
+  before_action :keychain_member_required, except: [:new, :create] # keep Third
+  # before_action :keychain_must_be_admin, only: [:update, :edit, :destory]
+  before_action :check_admin_create, only: :create
 
   def show; end
 
   def new; end
 
-  # TODO: create update (must have admin) edit (must have admin) destroy (must have admin)
+  def create
+    @keychain = Keychain.new(params.require(:keychain).permit(:name))
+    if @keychain.save
+      admins = [@user,
+                User.find_by(email: params[:email_1]&.capitalize),
+                User.find_by(email: params[:email_2]&.capitalize),
+                User.find_by(email: params[:email_3]&.capitalize),
+                User.find_by(email: params[:email_4]&.capitalize)].compact
+      admins.each do |person|
+        # TODO: Error invite not sticking or showing up as expected.
+        mem = @keychain.invite(person, true, false)
+        mem.accept if @user == person # creator of keychain is auto accepted
+      end
+      flash[:notice] = ['Keychain Completed']
+      return redirect_to keychain_path(@keychain)
+    end
+    flash[:alert] = @keychain.errors.full_messages
+    redirect_to new_keychain_path, status: :bad_request
+  end
+
+  # TODO: update (must have admin) edit (must have admin) destroy (must have admin)
 
   private
 
   # Determines if user is an active member of that keychain and sets @membership
   def keychain_member_required
-    @membership = @keychain.active_members.find { |mem| mem.user == @user }
+    @membership = @keychain&.active_members&.find { |mem| mem.user == @user }
     return if @membership
 
     flash[:alert] = ['Not an active member of that keychain']
@@ -37,5 +58,18 @@ class KeychainController < ApplicationController
 
     flash[:alert] = ['You must be an admin of the keychain to proceed.']
     redirect_to login_path, status: :unauthorized
+  end
+
+  def check_admin_create
+    list = params.require(:keychain).permit(:name, :email_1, :email_2, :email_3, :email_4)
+    email = []
+    email << 'Email 1 is not a valid user' unless list[:email_1] == '' || User.exists?(email: list[:email_1].capitalize)
+    email << 'Email 2 is not a valid user' unless list[:email_2] == '' || User.exists?(email: list[:email_2].capitalize)
+    email << 'Email 3 is not a valid user' unless list[:email_3] == '' || User.exists?(email: list[:email_3].capitalize)
+    email << 'Email 4 is not a valid user' unless list[:email_4] == '' || User.exists?(email: list[:email_4].capitalize)
+    return if email.empty?
+
+    flash[:alert] = email
+    redirect_to new_keychain_path, status: :bad_request
   end
 end
